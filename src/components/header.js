e43479e79529266d094b092436844b7524fac7a9
MitchGianoni <mitchgianoni@yahoo.com>
2018-10-27 17:10:00 -0400

    fixed avatar image styles

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,9 @@ export default class Header extends React.Component {
   render() {
     return (
       <section className="header-bar row">
-        <img src="https://s3.us-east-2.amazonaws.com/mg-icons/Mitch.jpg" className="avatar" alt="avatar" title="Avatar"></img>
+        <section class="circular-landscape">
+          <img src="https://s3.us-east-2.amazonaws.com/mg-icons/Mitch.jpg" className="avatar" alt="avatar" title="Avatar"></img>
+        </section>
         <Bio className=""/>
       </section>
     );
