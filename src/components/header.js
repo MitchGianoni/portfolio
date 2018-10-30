84be36e3cc2b0a8701bbbe582a67910cc4503a62
MitchGianoni <mitchgianoni@yahoo.com>
2018-10-30 09:39:23 -0400

    fix error

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <section className="header-bar row">
-        <section class="circular-landscape">
+        <section className="circular-landscape">
           <img src="https://s3.us-east-2.amazonaws.com/mg-icons/Mitch.jpg" className="avatar" alt="avatar" title="Avatar"></img>
         </section>
         <Bio className=""/>
